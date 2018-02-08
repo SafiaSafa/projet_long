414d2e7041b0f24186daff7ba72069427130ad3d
netbook <safiasafa@hotmail.fr>
2018-02-08 10:07:36 +0100

    result chgt

--- src/clustering.R
+++ src/clustering.R
@@ -8,15 +8,16 @@ library(ggfortify)
 library(factoextra) # clustering algorithms & visualization
 library(cluster)
 #kohonen library
-install.packages("kohonen")
+#install.packages("kohonen")
 library(kohonen)
-
+#install.packages(ProjectionBasedClustering)
+library(ProjectionBasedClustering)
 
 #Récupération des arguments
 variable <- commandArgs(trailingOnly=TRUE)
 
 #lecture du fichier csv
-#MyData <- read.csv(file="../result/test/vecteur_db.csv", header=TRUE, sep=";",stringsAsFactors=FALSE)
+#MyData <- read.csv(file="~/Bureau/projet_long/result/other/vecteur_db.csv", header=TRUE, sep=";",stringsAsFactors=FALSE)
 MyData <- read.csv(file=variable[1], header=TRUE, sep=";",stringsAsFactors=FALSE)
 
 MyData[is.na(MyData)] <- 0 #remplace les NA (s'il y en as) par des 0
@@ -34,12 +35,13 @@ MyData$vdw<-(MyData$vdw-mean(MyData$vdw))/sd(MyData$vdw)
 
 MyData$ionic<-(MyData$ionic-mean(MyData$ionic))/sd(MyData$ionic)
 
-#PCA
-  #center = TRUE,scale. = TRUE
+##############################################################PCA
+################################""ACP
+
 MyData.Vector <- MyData[, 2:length(MyData)]
 MyData.AA <- MyData$AA
 MyData.Vector.pca <- prcomp(MyData.Vector)
-#a="result/test/"
+
 a=variable[2]
 pdf(paste(a,"cluster.pdf"))
 plot(MyData.Vector.pca, type = "l",main="Variances en fonction des Composantes Principales")
@@ -55,33 +57,44 @@ fviz_pca_var(MyData.Vector.pca, col.var = "cos2",
              repel = TRUE # Évite le chevauchement de texte
 )
 
-  #clusterisation
-  #kmeans
-test <-function(n,v){
-polar=rownames(table(n, kmeans.result$cluster))
-polar=lapply(polar, as.numeric)
-toto = table(n, kmeans.result$cluster)
-test=matrix(nrow=1, ncol=dim(toto)[2])
-for (i in 1:dim(toto)[2])
-{
-  mat= 0
-  for(j in 1:dim(toto)[1]){
-    coeff=toto[j,i]
-    a=coeff*as.numeric(polar[i])
-    mat=mat+a
-    
-  }
-  test[i]=mat
-}
-sum_polar=sum(test[1,])
-for (i in 1:dim(test)[2]){
-  test[,i]=(as.numeric(test[,i])*100)/sum_polar
-}
-colnames(test)=colnames(nb_obs)
-barplot(test,ylab=v,xlab="Cluster", main= paste(v,"\n Kmeans"))
 
-return(0)
+################################MDS
+MDS(as.matrix(MyData.Vector),method='euclidean',OutputDimension=2,PlotIt=TRUE)
+
+################################SAMMON
+#SammonsMapping(as.matrix(MyData.Vector),method= 'euclidean',OutputDimension=2,PlotIt=TRUE)
+
+###############################################  #clusterisation
+######################################## #kmeans
+pourcentage <-function(n,v,y,z){
+  polar=rownames(table(n, z$cluster))
+  polar=lapply(polar, as.numeric)
+  toto = table(n, z$cluster)
+  test=matrix(nrow=1, ncol=dim(toto)[2])
+  for (i in 1:dim(toto)[2])
+  {
+    mat= 0
+    for(j in 1:dim(toto)[1]){
+      coeff=toto[j,i]
+      a=coeff*abs(as.numeric(polar[i]))
+      mat=mat+a
+      
+    }
+    test[i]=mat
+  }
+
+  sum_polar=sum(test[1,])
+
+  for (i in 1:dim(test)[2]){
+    test[,i]=(abs(as.numeric(test[,i]))*100)/abs(sum_polar)
+  }
+  colnames(test)=colnames(nb_obs)
+  barplot(test,ylab=v,xlab="Cluster", main= paste(v,y),ylim=c(0,100))
+  
+  return(0)
 }
+
+
     #DETERMINATION NB CLUSTER
 nb_cluster=5
 fviz_nbclust(MyData.Vector, kmeans, method = "wss")+
@@ -126,41 +139,15 @@ print(mat)
 #par(mfrow=c(2,dim(mat)[2]/2))
 for (i in 1:dim(mat)[2])
 {
-  barplot(mat[,i],main='Distribution des scores (Méthodes Kmeans)',xlab=paste("Cluster ",i),ylab="Score",ylim=c(0,-10))
+  barplot(mat[,i],main='Distribution des scores (Méthodes Kmeans)',xlab=paste("Cluster ",i),ylab="Score",ylim=c(4,-4))
 }
-test(MyData$ACC,"Accessibilité au solvant (%)")
-test(MyData$polar,"% d'interaction polar")
-test(MyData$ionic," % d'interaction ionic")
-test(MyData$hydrophobic," % d'interaction hydrophobic")
-test(MyData$vdw," % d'interaction van der Waals")
-test(MyData$hydrogenB," % de liaisons hydrogènes")
-  #kmedoid
-    #Clustering with pam()
-test <-function(n,v){
-polar=rownames(table(n, pam.res$cluster))
-polar=lapply(polar, as.numeric)
-toto = table(n, pam.res$cluster)
-test=matrix(nrow=1, ncol=dim(toto)[2])
-for (i in 1:dim(toto)[2])
-{
-  mat= 0
-  for(j in 1:dim(toto)[1]){
-    coeff=toto[j,i]
-    a=coeff*as.numeric(polar[i])
-    mat=mat+a
-    
-  }
-  test[i]=mat
-}
-sum_polar=sum(test[1,])
-for (i in 1:dim(test)[2]){
-  test[,i]=(as.numeric(test[,i])*100)/sum_polar
-}
-colnames(test)=colnames(nb_obs)
-barplot(test,ylab=v,xlab="Cluster", main= paste(v,"\n Kmedoid"))
 
-return(0)
-}
+
+
+
+######################################################  #kmedoid
+    #Clustering with pam()
+
 
 pam.res=pam(MyData.Vector, nb_cluster)
 autoplot(pam.res, frame = TRUE, frame.type = 'norm',main="Clustering par Kmedoid(PAM)")
@@ -201,15 +188,22 @@ print(mat)
 #par(mfrow=c(2,dim(mat)[2]/2))
 for (i in 1:dim(mat)[2])
 {
-  barplot(mat[,i],main='Distribution des scores (Méthode PAM)',xlab=paste("Cluster ",i),ylab="Score",ylim=c(0,-10))
+  barplot(mat[,i],main='Distribution des scores (Méthode PAM)',xlab=paste("Cluster ",i),ylab="Score",ylim=c(4,-4))
 }
-test(MyData$ACC,"Accessibilité au solvant (%)")
-test(MyData$polar,"% d'interaction polar")
-test(MyData$ionic," % d'interaction ionic")
-test(MyData$hydrophobic," % d'interaction hydrophobic")
-test(MyData$vdw," % d'interaction van der Waals")
-test(MyData$hydrogenB," % de liaisons hydrogènes")
-
+########################################
+pourcentage(MyData$ACC,"Accessibilité au solvant (%)","Kmeans",kmeans.result)
+pourcentage(MyData$polar,"% d'interaction polar","Kmeans",kmeans.result)
+pourcentage(MyData$ionic," % d'interaction ionic","Kmeans",kmeans.result)
+pourcentage(MyData$hydrophobic," % d'interaction hydrophobic","Kmeans",kmeans.result)
+pourcentage(MyData$vdw," % d'interaction van der Waals","Kmeans",kmeans.result)
+pourcentage(MyData$hydrogenB," % de liaisons hydrogènes","Kmeans",kmeans.result)
+#####################################
+pourcentage(MyData$ACC,"Accessibilité au solvant (%)","Kmedoid",pam.res)
+pourcentage(MyData$polar,"% d'interaction polar","Kmedoid",pam.res)
+pourcentage(MyData$ionic," % d'interaction ionic","Kmedoid",pam.res)
+pourcentage(MyData$hydrophobic," % d'interaction hydrophobic","Kmedoid",pam.res)
+pourcentage(MyData$vdw," % d'interaction van der Waals","Kmedoid",pam.res)
+pourcentage(MyData$hydrogenB," % de liaisons hydrogènes","Kmedoid",pam.res)
 
 
 ###########################################"kohonen"
@@ -233,7 +227,7 @@ print(carte$grid)
 
 
 #count plot
-plot(carte,type="count",palette.name=degrade.bleu)
+plot(carte,type="count",palette.name=degrade.bleu,main=paste("Carte de Kohonen n=",n))
 
 #noeud d’appartenance des observations
 print(carte$unit.classif)
@@ -251,17 +245,22 @@ plot(carte,type="codes",codeRendering = "segments")
 #tableaudes codebooks pour les deux premiers noeuds
 print(carte$codes[[1]][1:2,]) 
 
-#graphique pour chaque variable
-par(mfrow=c(6,4))
-for (j in 1:ncol(MyData.Vector)){
-plot(carte,type="property",property=carte$codes[[1]][,j],palette.name=coolBlueHotRed,main=colnames(MyData.Vector)[j],cex=0.5)
-}
+
+
 par(mfrow=c(1,1))
 
 return(0)
 }
-test(2)
------------------------------------toroidal-----------------------------
+
+
+test(4)
+
+test(9)
+
+test(16)
+
+test(25)
+#################################toroidal-----------------------------
 thor<-function(n){
 set.seed(100)
 carte_tor <-som(as.matrix(MyData.Vector),grid=somgrid(xdim = n, ydim = n, topo =  "hexagonal",
@@ -272,7 +271,7 @@ print(summary(carte_tor))
 print(carte_tor$grid)
 
 #count plot
-plot(carte_tor,type="count",palette.name=degrade.bleu)
+plot(carte_tor,type="count",palette.name=degrade.bleu,main=paste("Carte de Kohonen toroidal n=",n))
 
 #noeud d’appartenance des observations
 print(carte_tor$unit.classif)
@@ -290,14 +289,14 @@ plot(carte_tor,type="codes",codeRendering = "segments")
 #tableaudes codebooks pour les deux premiers noeuds
 print(carte_tor$codes[[1]][1:2,]) 
 
-#graphique pour chaque variable
-par(mfrow=c(6,4))
-for (j in 1:ncol(MyData.Vector)){
-plot(carte_tor,type="property",property=carte_tor$codes[[1]][,j],palette.name=coolBlueHotRed,main=colnames(MyData.Vector)[j],cex=0.5)
-}
-par(mfrow=c(1,1))
-
 return(0)
 }
-thor(2)
+
+thor(4)
+
+thor(9)
+
+thor(16)
+
+thor(25)
 dev.off()
